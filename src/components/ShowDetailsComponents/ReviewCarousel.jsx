b012a615dbe1e772bb6f323f441163e2da16263e
momen186 <momenehab186@gmail.com>
2025-06-01 18:47:43 +0300

    modularized slider and reviews

--- src/components/ShowDetailsComponents/ReviewCarousel.jsx
+++ src/components/ShowDetailsComponents/ReviewCarousel.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import StarRating from './StarRating';
-import './Style/TvShowReview.css';
+import './Style/ReviweCarousel.css'; // <-- New, shared CSS
 
-const TvShowReview = ({ reviews }) => {
+const ReviewCarousel = ({ reviews, title }) => {
   const [index, setIndex] = useState(0);
   const [hovered, setHovered] = useState(false);
-
 
   // Group reviews into chunks of 4
   const chunkSize = 4;
@@ -24,14 +23,14 @@ const TvShowReview = ({ reviews }) => {
     setIndex(selectedIndex);
   };
 
-  // Custom arrow components
+  // Custom arrow components (show on hover)
   const CustomPrevArrow = ({ onClick }) => (
-    <div 
+    <div
       className="carousel-arrow carousel-arrow-left"
       onClick={onClick}
       style={{
         opacity: hovered ? 1 : 0,
-        transition: 'opacity 0.3s ease'
+        transition: 'opacity 0.3s ease',
       }}
     >
       <FaChevronLeft size={24} />
@@ -39,12 +38,12 @@ const TvShowReview = ({ reviews }) => {
   );
 
   const CustomNextArrow = ({ onClick }) => (
-    <div 
+    <div
       className="carousel-arrow carousel-arrow-right"
       onClick={onClick}
       style={{
         opacity: hovered ? 1 : 0,
-        transition: 'opacity 0.3s ease'
+        transition: 'opacity 0.3s ease',
       }}
     >
       <FaChevronRight size={24} />
@@ -52,23 +51,27 @@ const TvShowReview = ({ reviews }) => {
   );
 
   return (
-    <div 
-      className="reviews-slider-container" 
+    <div
+      className="reviews-slider-container"
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       style={{
         position: 'relative',
         padding: '0 3em',
-        margin: '2em 0'
+        margin: '2em 0',
       }}
     >
-      <h4 className="mb-4">User Reviews</h4>
-      
-      <CustomPrevArrow onClick={() => handleSelect(index - 1 < 0 ? reviewGroups.length - 1 : index - 1)} />
-      
-      <Carousel 
-        activeIndex={index} 
-        onSelect={handleSelect} 
+      <h4 className="mb-4">{title}</h4>
+
+      <CustomPrevArrow
+        onClick={() =>
+          handleSelect(index - 1 < 0 ? reviewGroups.length - 1 : index - 1)
+        }
+      />
+
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
         indicators={false}
         controls={false}
         interval={null}
@@ -77,20 +80,26 @@ const TvShowReview = ({ reviews }) => {
           <Carousel.Item key={groupIndex}>
             <div className="row" style={{ margin: '0 -0.5em' }}>
               {group.map((result) => (
-                <div className="col-md-3 col-sm-6 col-12" key={result.id} style={{ padding: '0 0.5em' }}>
+                <div
+                  className="col-md-3 col-sm-6 col-12"
+                  key={result.id}
+                  style={{ padding: '0 0.5em' }}
+                >
                   <div className="review-card">
                     {/* Avatar Section */}
                     <div className="review-avatar">
                       {result.author_details.avatar_path ? (
-                        <img 
-                          src={`https://image.tmdb.org/t/p/w64_and_h64_face${result.author_details.avatar_path}`} 
+                        <img
+                          src={`https://image.tmdb.org/t/p/w64_and_h64_face${result.author_details.avatar_path}`}
                           alt={result.author_details.username}
                           className="avatar-image"
                         />
                       ) : (
                         <i className="bi bi-person-circle avatar-fallback"></i>
                       )}
-                      <span className="review-username">{result.author_details.username}</span>
+                      <span className="review-username">
+                        {result.author_details.username}
+                      </span>
                     </div>
 
                     {/* Rating Section */}
@@ -104,8 +113,8 @@ const TvShowReview = ({ reviews }) => {
                     {/* Content Section */}
                     <div className="review-content">
                       <p>
-                        {result.content.length > 150 
-                          ? `${result.content.substring(0, 150)}...` 
+                        {result.content.length > 150
+                          ? `${result.content.substring(0, 150)}...`
                           : result.content}
                       </p>
                       <Link to={result.url} className="review-link">
@@ -120,11 +129,11 @@ const TvShowReview = ({ reviews }) => {
         ))}
       </Carousel>
 
-      <CustomNextArrow onClick={() => handleSelect((index + 1) % reviewGroups.length)} />
-
-
+      <CustomNextArrow
+        onClick={() => handleSelect((index + 1) % reviewGroups.length)}
+      />
     </div>
   );
 };
 
-export default TvShowReview;
+export default ReviewCarousel;
